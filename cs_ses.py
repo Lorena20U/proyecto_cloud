d2699b4bb20d15c0faebc052c6e396fbe4f18e7e
Lorena20U <lorenaperez@ufm.edu>
2023-10-24 20:14:42 -0600

    api-gw

--- cs_ses.py
+++ cs_ses.py
@@ -4,69 +4,14 @@ from email.mime.text import MIMEText
 import smtplib
 from dotenv import load_dotenv
 import os
+import requests
 
 def sender_ses(recipent, categoria, creador, proyecto_actual):
-    load_dotenv()
+    # The API endpoint
+    url = f"https://n0g86wyeq5.execute-api.us-east-1.amazonaws.com/app?recipent={recipent}&categoria={categoria}&creador={creador}&proyecto_actual={proyecto_actual}"
+    print(url)
+    # A GET request to the API
+    response = requests.get(url)
 
-    SENDER = "yaneth33612@gmail.com"
-    SENDERNAME = "Co&Co"
-    RECIPIENT = recipent
-    USERNAME_SMTP = os.getenv("USERNAME_SMTP")
-    PASSWORD_SMTP = os.getenv("PASSWORD_SMTP")
-    HOST = "email-smtp.us-east-1.amazonaws.com"
-    PORT = 587
-
-    SUBJECT = "Se agregó una nueva conferencia. - CO&CO"
-    BODY_TEXT = f"Te saludamos desde CO&CO, \n\nSe ha agregado un proyecto nuevo en las siguientes áreas de tu interés:  {categoria}. \n\nNombre: {proyecto_actual['name']}\nObjetivo: {proyecto_actual['objetivo']}\nFecha_cierre: {proyecto_actual['fecha_cierre']}\nDescripcion: \n{proyecto_actual['descripcion']}\n \nPara más información contacta a:\n{creador['name']} {creador['last_name']}\n{creador['e_mail']}\n{creador['celphone']}\n\n\nIngresa a CO&CO para enterarte de las últimas funciones."
-
-    BODY_TEXT = ("Amazon SES Test (Python)\r\n"
-                "This email was sent with Amazon SES using the "
-                "AWS SDK for Python (Boto)."
-                )
-                
-    BODY_HTML = f"""<html>
-    <head></head>
-    <body>
-    <h1>Te saludamos desde CO&CO</h1>
-    <p>Se ha agregado un proyecto nuevo en las siguientes áreas de tu interés:  {categoria}.
-    <br> Nombre: {proyecto_actual['name']}
-    <br> Objetivo: {proyecto_actual['objetivo']}
-    <br> Fecha_cierre: {proyecto_actual['fecha_cierre']}
-    <br> Descripcion: 
-    <br> {proyecto_actual['descripcion']}
-    <br> Para más información contacta a:
-    <br> {creador['name']} {creador['last_name']}
-    <br> {creador['e_mail']}
-    <br> {creador['celphone']}
-    <br> Ingresa a CO&CO para enterarte de las últimas funciones.
-    </p>
-    </body>
-    </html>
-                """            
-
-    msg = MIMEMultipart('alternative')
-    msg['From'] = email.utils.formataddr((SENDERNAME, SENDER))
-    # msg['To'] = RECIPIENT
-    msg['Subject'] = SUBJECT
-
-    part1 = MIMEText(BODY_TEXT, 'plain')
-    part2 = MIMEText(BODY_HTML, 'html')
-    msg.attach(part1)
-    msg.attach(part2)
-
-    try:
-        server = smtplib.SMTP(HOST, PORT)
-        server.ehlo()
-        server.starttls()
-        server.ehlo()
-        server.login(USERNAME_SMTP, PASSWORD_SMTP)
-        # server.sendmail(SENDER, RECIPIENT, msg.as_string())
-        for correo in recipent: 
-            server.sendmail(SENDER, correo, msg.as_string())
-            print(f"Sent! : {correo}")
-
-        server.close()
-    except Exception as e:
-        print("Error :( ... ", e)
-    else:
-        print("Email sent!")
+    # Print the response
+    response_json = response.json()
