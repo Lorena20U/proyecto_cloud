7894ba28b56ace51bc7a9bfe14c290039af23c19
Lorena20U <lorenaperez@ufm.edu>
2023-10-30 17:13:49 -0600

    arg

--- cs_ses.py
+++ cs_ses.py
@@ -8,7 +8,8 @@ import requests
 
 def sender_ses(recipent, categoria, creador, proyecto_actual):
     # The API endpoint
-    url = f"https://n0g86wyeq5.execute-api.us-east-1.amazonaws.com/app?recipent={recipent}&categoria={categoria}&creador={creador}&proyecto_actual={proyecto_actual}"
+    # url = f"https://n0g86wyeq5.execute-api.us-east-1.amazonaws.com/app?recipent={recipent}&categoria={categoria}&creador={creador}&proyecto_actual={proyecto_actual}"
+    url = f"{os.environ['API_URL']}?recipent={recipent}&categoria={categoria}&creador={creador}&proyecto_actual={proyecto_actual}"
     print(url)
     # A GET request to the API
     response = requests.get(url)
